c3132a0710a826324aaa1297702e6af9af323ca0
apcs11839 <apcs11839@10.29.8.51>
2018-09-13 08:24:59 +0800

    Finished work. All tasks today finished.

--- DoNow091318/src/Question3.java
+++ DoNow091318/src/Question3.java
@@ -6,7 +6,15 @@ public class Question3
 
 	public static void main(String[] args) 
 	{
-		
+		printInversely("Printed Inversely!!!");//usage example
 	}
 
+	public static void printInversely(String a)
+	{
+		for(int i=a.length();i>=1;i--)
+		{
+			System.out.print(a.substring(i-1, i));
+		}
+	}
 }
+
