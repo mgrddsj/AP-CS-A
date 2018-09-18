73fa776591390d23da0285204b512b790b051a8d
apcs11839 <apcs11839@10.29.8.13>
2018-09-18 08:10:42 +0800

    Today's project.

---
+++ DoNow091818/src/QUESTION1.java
@@ -0,0 +1,17 @@
+//A method that prints two 
+public class QUESTION1 
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+
+	
+	public static void combine(String string1,String string2)
+	{
+		System.out.println(string1 + string2);
+	}
+	
+	
+}
