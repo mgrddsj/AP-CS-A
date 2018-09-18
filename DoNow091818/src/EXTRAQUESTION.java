5de0822488fa9e6f2a6e04435581ffed50892955
apcs11839 <apcs11839@10.29.8.13>
2018-09-18 08:48:36 +0800

    Extra question 20180918

---
+++ DoNow091818/src/EXTRAQUESTION.java
@@ -0,0 +1,22 @@
+
+public class EXTRAQUESTION 
+{
+
+	public static void main(String[] args) 
+	{
+		isReverseTheSame("MIMO");
+		isReverseTheSame("MOM");
+		isReverseTheSame("MOOM");
+	}
+
+	public static void isReverseTheSame(String text)
+	{
+		String inverse = "";
+		for(int i=text.length()-1;i>=0;i--)
+		{
+			inverse = inverse + text.charAt(i);
+		}
+		
+		System.out.println(text.equals(inverse));
+	}
+}
