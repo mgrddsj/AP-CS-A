e7eeaa532a74ee85fd095a2abd44446f46e770e9
apcs11839 <apcs11839@10.29.8.23>
2018-09-20 08:49:50 +0800

    Today's work 2018.09.20

---
+++ DoNow092018/src/METHODSANDRETURN.java
@@ -0,0 +1,38 @@
+
+
+
+public class METHODSANDRETURN 
+{
+
+	public static void main(String[] args) 
+	{
+		question1();
+		System.out.println(question2());
+		question3("Ooooooo",123123);
+		System.out.println(question4("haha","hehe"));
+	}
+
+	//Question (a)
+	public static void question1()
+	{
+		
+	}
+	
+	//Question (b)
+	public static String question2()
+	{
+		return "Hello World!";
+	}
+	
+	//Question (c)
+	public static void question3(String text,int a)
+	{
+		
+	}
+	
+	//Question (d)
+	public static double question4(String text1,String text2)
+	{
+		return 1234567890;
+	}
+}
