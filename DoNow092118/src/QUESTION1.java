ce4db96bacf54f9f741eeb4ebaa05fcb1855522b
apcs11839 <apcs11839@10.29.8.152>
2018-09-21 08:20:41 +0800

    Today's work 2018.09.21.

---
+++ DoNow092118/src/QUESTION1.java
@@ -0,0 +1,14 @@
+
+public class QUESTION1 
+{
+
+	public static void main(String[] args) 
+	{
+		compareTwoValues(12,23);
+	}
+
+	public static void compareTwoValues(int a,int b)
+	{
+		System.out.println(Math.max(a, b));
+	}
+}
---
+++ DoNow092118/src/QUESTION3.java
@@ -0,0 +1,14 @@
+
+public class QUESTION3 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(analysis("Sample","Usage"));
+	}
+
+	public static String analysis(String text1,String text2)
+	{
+		return "String " + text1 + " has length of " + text1.length() + " and string " + text2 + " has length of " + text2.length();
+	}
+}
