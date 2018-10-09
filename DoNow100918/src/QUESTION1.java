732d46faa91cc24e06380d84ff7fb62bf9611301
apcs11839 <apcs11839@10.29.8.18>
2018-10-09 08:51:02 +0800

    Finished work. Added prime number practice.

---
+++ DoNow100918/src/QUESTION1.java
@@ -0,0 +1,29 @@
+
+public class QUESTION1 
+{
+
+	public static void main(String[] args) 
+	{
+		prime(100);
+	}
+
+	public static int countFactors(int num)
+	{
+		int numOfFactors=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+				numOfFactors++;
+		}
+		return numOfFactors;
+	}
+	
+	public static void prime(int max)
+	{
+		for(int i=2;i<=max;i++)
+		{
+			if (countFactors(i)==2)
+				System.out.print(i + " ");
+		}
+	}
+}
