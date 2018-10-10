7b1afeb823f3628741476a07040727aed1e68234
apcs11839 <apcs11839@10.29.8.18>
2018-10-10 08:50:28 +0800

    Exercise 4 unfinished.

---
+++ DoNow101018-Exercises/src/EXERCISES4.java
@@ -0,0 +1,22 @@
+//Exercise 4 on pg.309
+public class EXERCISES4 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+	public static int daysInMonth(int month)
+	{
+		if (month==2)
+			return 28;
+		else
+		{
+			if (month<=7)
+			{
+				
+			}
+		}
+	}
+}
