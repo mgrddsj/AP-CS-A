ee149780c51c3f98d4640f40bc2d9b09e0d8cb49
apcs11839 <apcs11839@10.29.8.33>
2018-10-12 08:46:04 +0800

    Today's work. Day in week calculator not finished.

--- DoNow101018-Exercises/src/EXERCISES6.java
+++ DoNow101018-Exercises/src/EXERCISES6.java
@@ -1,9 +1,11 @@
+//Exercise 6 on pg.310
+
 
 public class EXERCISES6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
+
 
 }
---
+++ DoNow101218/src/PROGRAMMINGPROJECT2_PG313.java
@@ -0,0 +1,14 @@
+//Day of week from date
+
+
+public class PROGRAMMINGPROJECT2_PG313 {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static int dayOfWeek(int month,int day,int year)
+	{
+		
+	}
+}
