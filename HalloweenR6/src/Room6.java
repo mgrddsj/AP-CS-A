9f1c15c44d1050e050be498e5af13d24a1c21b7a
Jesse Xu <Jesse_Xu@live.com>
2018-10-25 18:40:20 +0800

    Room 6 and 7 finished

---
+++ HalloweenR6/src/Room6.java
@@ -0,0 +1,152 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Room6 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		printw(
+				"When time stops \r\n" + 
+				"When words fade \r\n" + 
+				"Only light will guide the way \r\n" + 
+				"For it will meet its reflection\r\n" + 
+				"Face to face\r\n" + 
+				"Hours and minutes\r\n" + 
+				"When the Track reveal itself\r\n" + 
+				"Lights will find its way");
+		
+		password();
+	}
+
+	public static void printw(String text) throws InterruptedException
+	{
+		for (int i=0;i<=text.length()-1;i++)
+		{
+			if (text.charAt(i)=='\n')
+				Thread.sleep(150);
+			System.out.print(text.charAt(i));
+			Thread.sleep(25);
+		}
+	}
+	
+	public static void printfast(String text) throws InterruptedException
+	{
+		for (int i=0;i<=text.length()-1;i++)
+		{
+			if (text.charAt(i)=='\n')
+				Thread.sleep(100);
+			System.out.print(text.charAt(i));
+			Thread.sleep(5);
+		}
+	}
+	
+	public static void wait(int time) throws InterruptedException
+	{
+		Thread.sleep(time);
+	}
+	
+	public static void password() throws InterruptedException
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("\n\nPassword: ");
+		String input = in.nextLine();
+		if (input.equalsIgnoreCase("HARD"))
+		{
+				accessGranted();
+				Thread.sleep(1000);
+				hiddenMessage();
+		}
+		else if (input.equalsIgnoreCase("EASY"))
+			{
+				accessGranted();
+				wait(2000);
+				advancePassword();
+			}
+		else 
+		{
+			printw("Wrong! Try again! ");
+			password();
+		}
+	}
+	
+	public static void accessGranted() throws InterruptedException
+	{
+		System.out.println("Decrypting...");
+		wait(2000);
+		System.out.println("Document from the United Nations: \n");
+		printfast(
+				"To the General Assembly:\r\n" + 
+				"Reminding the danger of the misanthropist (hate-human) organizations named \"Anthropos Conrumpo\";\r\n" + 
+				"Acknowledging the disease that they spread is dangerous;\r\n" + 
+				"Noting with regret that the WHO didn't notice it on time;\r\n" + 
+				"1. Urges every country to escort(send) healthy people into the underground bunker,\r\n" + 
+				"    a. Reminding them that the S.I.M. simulation is usable\r\n" + 
+				"2. Recommends every country to find a way to upload conscious into simulation,\r\n" + 
+				"3. Urges people to stay in the simulation until a cure is found, outside or inside.\n\n");
+		
+		System.out.println("There's another document from an unknown source, press enter to open it. ");
+		Scanner in = new Scanner(System.in);
+		in.nextLine();
+		
+		printfast(
+				"Adna Loy's diary:\r\n" + 
+				"Well. The UN didn't expect that a mole--a betrayer, has been hiding in them the whole time!\r\n" + 
+				"As a high member of the AC organization, I will stop them and stop the effort in erasing our\r\n" + 
+				"virus away!\r\n" + 
+				"Here's my plan: I will upload a virus to the simulation: After 4 people was uploaded in, \r\n" + 
+				"their memories for 3 years will be erased, \r\n" + 
+				"and the ¡°island¡± will be changed into an ¡°abandoned building. \r\n" + 
+				"Then, the machine will go haywire, preventing others to get in.\r\n" + 
+				"\r\n" + 
+				"I will will trick them into destroying the simulation in order to escape¡­\r\n" + 
+				" that way, no cure will be found. I have already ordered the people infected to destroy all labs on Earth.\r\n" + 
+				"\r\n" + 
+				"No one can stop us now!\n\n");
+		
+		
+	}
+	
+	public static void hiddenMessage() throws InterruptedException
+	{
+		printw("You have received a new ingredient: Blood. \n");
+	}
+	
+	public static void randomBinaries() throws InterruptedException
+	{
+		Random rand = new Random();
+		for (int a=1;a<=10;a++)
+		{
+			for (int i=1;i<=100;i++)
+			{
+				System.out.print(rand.nextInt(2));
+				Thread.sleep(2);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void advancePassword() throws InterruptedException
+	{
+		wait(1000);
+		printw("\n\nThere's an advanced password. You have 5 chances to try it. \n");
+		Scanner in = new Scanner(System.in);
+		
+		for (int trials=1;trials<=5;trials++)
+		{
+			System.out.print("Password: ");
+			if (in.next().equalsIgnoreCase("2021"))
+			{
+				hiddenMessage();
+				break;
+			}
+			else
+			{
+				if (trials!=5)
+					printw("Wrong! Try again! You have " + (5-trials) + " more chances. \n");
+				else 
+					printw("Wrong! Access denied! \n");
+			}
+		}
+	}
+}
---
+++ HalloweenR7/src/Room7.java
@@ -0,0 +1,181 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Room7 
+{
+
+	public static void main(String[] args) throws InterruptedException, IOException 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Start playing by pressing enter. ");
+		in.nextLine();
+		new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+		
+		printw(
+				"SIMULATION INFO TRANSPORTING AREA\r\n" + 
+				"\r\n" + 
+				"PASSCODE NEEDED TO TRANSMIT INFORMATION OUTSIDE\r\n");
+		
+		printw("HACKING.....\r\n");
+		wait(2000);
+		
+		printw("FOUND DOCUMENT AND DATA SNIPPET: \n\n");
+		printfast(
+				"In the year ____, a group of misanthropist scientists named ¡°______¡± felt that humans were the root \r\n" + 
+				"of all problems that are happening right now: global warming, mass extinction of flora and fauna, etc. \r\n" + 
+				"They thought that in order to save Earth, humans must be controlled by them in order to make things \r\n" + 
+				"right. In order to that, they secretly developed a virus ______ called ¡°______¡±; it targets the pre-frontal \r\n" + 
+				"cortex of the brain, making humans not capable of self-control and self-thought. By the time the \r\n" + 
+				"government and the WHO had discovered it, it was too late; 99% of the human population has been \r\n" + 
+				"infected. Why? The group controlled the people who are infected to ¡°act normal¡± until the whole \r\n" + 
+				"population is infected, so that no forces can stop them. There are no effect in first stage infection; you \r\n" + 
+				"cannot see any symptoms when infected.\r\n" + 
+				"\r\n" + 
+				"In order to preserve the rest of the healthy population, the healthy people from all around the world\r\n" + 
+				" meet up together to deliver a plan: to send the healthy population to an underground bunker to \r\n" + 
+				"isolate them. They plan to ______ made one month ago---it was actually used to cure psychological \r\n" + 
+				"diseases, so it simulated an island for the sick to rest. Looks like this is what it is now used. The people \r\n" + 
+				"will stay there until the cure is found.\r\n" + 
+				"\r\n" + 
+				"You are the first 4 people to upload your minds into the simulation. However, no one suspected a mole\r\n" + 
+				" in the group: a computer scientist named ¡°_____¡±, a high member of ¡°AC¡±. In order to stop this \r\n" + 
+				"effort to eradicate the disease, he installed a virus to the simulation: After 4 people was uploaded in, \r\n" + 
+				"their ____ will be erased, and the ¡°island¡± will be changed into an ¡°abandoned building¡±. Then, the \r\n" + 
+				"machine will go haywire, preventing others to get in.\r\n" + 
+				"\r\n" + 
+				"He will trick them into _____ in order to escape¡­ that way, no cure will be found. He has already ordered \r\n" + 
+				"the people infected to _____  on Earth.\r\n" + 
+				"\n");
+		
+		printfast(
+				"1.    A 2018   B 2020    C 2021  D 2022\r\n" + 
+				"2.    N Anthropos Condesendo  O Anthropos Conrumpo  P Animalia Condesendo  Q Animalia Conrumpo\r\n" + 
+				"3.    R control people¡¯s minds   S disable the function of their arms  T disable the function of their legs   U cause them to commit suicide\r\n" + 
+				"4.    O Teinaire   P Radiore   Q Potoriea  R Poitiore\r\n" + 
+				"5.    U upload them into a simulation   V send them into a space station   W stay in the bunker    X transport them into a secret lab\r\n" + 
+				"6.    O Edni Yam   P Adna Loy   Q Lola Day   R Snyde Ere\r\n" + 
+				"7.    R memories from 1 year ago  S abilities from 1 year ago T memories from 3 years ago   U abilities from 3 years ago\r\n" + 
+				"8.    D release a second virus   E destroy the simulation   F commit suicide   G destroy the lab\r\n" + 
+				"9.    A kill all the other scientists   B destroy all lab equipment   C destroy all electronic devices and transportation    D destroy all the labs\r\n" + 
+				"\n");
+		
+		password();
+		printw(
+				"PLEASE TRANSMIT INFO,\r\n" + 
+				"CURE INGREDIENTS:");
+		ingredient();
+		
+		wait(10000);
+		System.out.println("Press enter to exit. ");
+		in.nextLine();
+		in.close();
+	}
+
+	public static void printw(String text) throws InterruptedException
+	{
+		for (int i=0;i<=text.length()-1;i++)
+		{
+			if (text.charAt(i)=='\n')
+				Thread.sleep(500);
+			System.out.print(text.charAt(i));
+		}
+	}
+	
+	public static void printfast(String text) throws InterruptedException
+	{
+		for (int i=0;i<=text.length()-1;i++)
+		{
+			if (text.charAt(i)=='\n')
+				Thread.sleep(100);
+			System.out.print(text.charAt(i));
+			Thread.sleep(5);
+		}
+	}
+	
+	public static void wait(int time) throws InterruptedException
+	{
+		Thread.sleep(time);
+	}
+	
+	public static void Hack() throws InterruptedException
+	{
+		printw("HACK? (Y/N)\n");
+		
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		while (!input.matches("[y|n|Y|N]+"))
+		{
+			System.out.println("Please only input Y or N");
+			input = in.nextLine();
+		}
+		if (("" + input.charAt(0)).equalsIgnoreCase("n"))
+		{
+			Thread.sleep(500);
+			printw(". . . \n");
+			Thread.sleep(1000);
+			Hack();
+		}
+	}
+	
+	public static void password() throws InterruptedException
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("\nPASSCODE: ");
+		String input = in.nextLine();
+		if (input.equalsIgnoreCase("corrupted"))
+		{
+			printw("ACCESS GRANTED.\r\n");
+		}
+
+		else 
+		{
+			printw("Wrong! Try again! ");
+			password();
+		}
+	}
+	
+	public static void ingredient() throws InterruptedException
+	{
+		Scanner in = new Scanner(System.in);
+		int score = 0;
+		
+		System.out.print("1. ");
+		if (in.nextLine().equalsIgnoreCase("Uranium"))
+			score++;
+		
+		System.out.print("2. ");
+		if (in.nextLine().equalsIgnoreCase("Mercury"))
+			score++;
+		
+		System.out.print("3. ");
+		if (in.nextLine().equalsIgnoreCase("Bone"))
+			score++;
+		
+		System.out.print("4. ");
+		if (in.nextLine().equalsIgnoreCase("Wormwood"))
+			score++;
+		
+		System.out.print("5. ");
+		if (in.nextLine().equalsIgnoreCase("Zinc"))
+			score++;
+		
+		System.out.print("6. ");
+		if (in.nextLine().equalsIgnoreCase("Blood"))
+			score++;
+		
+		printw("TRANSMITTING......\n");
+		wait(2000);
+		printw("TRANSMITTED. \n");
+		System.out.println("ACCURACY: " + score + "/6");
+		
+		wait(1000);
+		printw(
+				"ESCAPE CODE: BA515\r\n" + 
+				"PLEASE TYPE IN ESCAPE CODE: ");
+		while (!in.nextLine().equalsIgnoreCase("BA515"))
+			System.out.println("Wrong password! Please try again! ");
+		
+		System.out.println("Exitting...");
+		printw("THANK YOU FOR USING S.I.M. SIMULATION. ");
+	}
+}
