c2f4dac298577137f3b94635a6034e016c14ebd8
Jesse Xu <Jesse_Xu@live.com>
2018-10-28 10:59:37 +0800

    The launchers for Halloween stuff.

---
+++ HalloweenLauncher/src/Launcher3.java
@@ -0,0 +1,54 @@
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Launcher3 
+{
+
+	public static void main(String[] args) throws IOException, Exception 
+	{
+		System.out.println("Please wait... Loading program...");
+		String processName = "room3.exe";
+		
+		while (true)
+		{
+			if (!isProcessRunning(processName))
+				open("D:\\Halloween\\Cmder - room3.lnk");
+			Thread.sleep(1000);
+		}
+	}
+
+	public static void open(String targetFilePath) throws IOException
+	{
+	    Desktop desktop = Desktop.getDesktop();
+
+	    desktop.open(new File(targetFilePath));
+	}
+	
+	private static final String TASKLIST = "tasklist";
+	private static final String KILL = "taskkill /F /IM ";
+
+	public static boolean isProcessRunning(String serviceName) throws Exception 
+	{
+
+	 Process p = Runtime.getRuntime().exec(TASKLIST);
+	 BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	 String line;
+	 while ((line = reader.readLine()) != null) 
+	 {
+		  if (line.contains(serviceName)) 
+			  return true;
+	 }
+
+	 return false;
+
+	}
+
+	public static void killProcess(String serviceName) throws Exception 
+	{
+	  Runtime.getRuntime().exec(KILL + serviceName);
+	}
+	
+}
