eb61209ef7210271a865a5c456cf841ae7ab6b1a
apcs11839 <apcs11839@10.29.8.39>
2018-10-29 08:48:58 +0800

    Today's work. PG386-Q7. 2018.10.29.

---
+++ DoNow102918/src/DONOW1.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//PG386-Q7
+public class DONOW1 
+{
+	public static int player1;
+	public static int player2;
+	
+	public static void main(String[] args) 
+	{
+		player1=0;
+		player2=0;
+		
+		while (player1<100&&player2<100)
+		{
+			pig(1);
+			if (player1>=100)
+				break;
+			else
+				pig(2);
+		}
+		
+		if (player1>=100)
+			System.out.println("Player 1 won. ");
+		else
+			System.out.println("Player 2 won. ");
+	}
+
+	public static void pig(int player)
+	{
+		Random rand = new Random();
+		Scanner in = new Scanner(System.in);
+		int score = 0;
+		int dice = 0;
+		System.out.println("Player " + player + ": ");
+		while (dice!=1)
+		{
+			System.out.print("Roll a die? ");
+			if (in.next().charAt(0)=='y')
+			{
+				dice = rand.nextInt(6)+1;
+				System.out.println("You got " + dice);
+				score+=dice;
+			}
+			else 
+			{
+				if (player==1)
+				{
+					player1+=score;
+					System.out.println("You got " + score + " this round. You have " + player1 + " points. ");
+				}
+				if (player==2)
+				{
+					player2+=score;
+					System.out.println("You got " + score + " this round. You have " + player2 + " points. ");
+				}
+				return;
+			}
+		}
+		System.out.println("You lost all the point this round! ");
+		return;
+	}
+}
---
+++ DoNow102918/src/NOTES.java
@@ -0,0 +1,10 @@
+/*
+ * Mr. Miro is not here today. Nobody knows what to do... 
+ */
+public class NOTES {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
