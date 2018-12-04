5e6326dc4dfb626f026064ee509b263f7be167cf
mgrddsj <Jesse_Xu@live.com>
2018-12-04 17:10:45 +0800

    Finished homework.

--- DoNow120418/src/twoDArray.java
+++ DoNow120418/src/twoDArray.java
@@ -37,4 +37,16 @@ public class twoDArray
 			System.out.println(Arrays.toString(list[i]));
 		}
 	}
+
+	public static void printColumnOrder(int[][] list)
+	{
+		for (int i=0;i<list.length;i++)
+		{
+			for (int j=0;i<list.length;j++)
+			{
+				System.out.print(list[j][i] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
