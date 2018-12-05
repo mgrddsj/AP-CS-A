6c89ecfc2aed9619615fdfa1e9b53b51469a6e05
mgrddsj <Jesse_Xu@live.com>
2018-12-05 08:17:28 +0800

    Today's work.
    Practice needs to be finished.

---
+++ DoNow120518/src/DoNow120518.java
@@ -0,0 +1,26 @@
+
+public class DoNow120518 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+	public static void question31()
+	{
+		/*
+		 * 0, 1, 2, 3
+		 * 1, 2, 3, 4
+		 * 2, 3, 4, 5
+		 */
+	}
+	
+	public static void question32(int[][] data)
+	{
+		for (int i=0;i<data[2].length;i++)
+		{
+			data[2][i] = i+1;
+		}
+	}
+}
---
+++ DoNow120518/src/PracticePG524Q13.java
@@ -0,0 +1,10 @@
+
+public class PracticePG524Q13 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
