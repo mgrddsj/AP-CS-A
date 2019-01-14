212c8c628fcbe57474d2b69c47b3f40a62744515
mgrddsj <Jesse_Xu@live.com>
2019-01-14 08:00:48 +0800

    Update Account.java

--- DoNow011119_CorrectDate/src/Account.java
+++ DoNow011119_CorrectDate/src/Account.java
@@ -2,24 +2,24 @@ import java.math.BigDecimal;
 
 public class Account 
 {
-	private BigDecimal value = new BigDecimal(0);
+    private double value;
     private String owner;
     
     public Account(String initOwner,double initValue)
     {
-    	BigDecimal initValueBig = new BigDecimal(initValue);
-    	value = BigDecimal.add(initValueBig);
+        BigDecimal value = new BigDecimal(initValue);
+        value = initValue;
         owner = initOwner;
     }
 
     public void deposit(double in)
     {
-        value = value.add(in);
+        value += in;
     }
 
     public void takeOut(double out)
     {
-        value = value.subtract(out);
+        value -= out;
     }
 
     public double check()
