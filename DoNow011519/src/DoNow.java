ab8f4f52b225e3c62883a53a71489f6a2ac20d9d
mgrddsj <Jesse_Xu@live.com>
2019-01-15 08:48:26 +0800

    Update DoNow.java

--- DoNow011519/src/DoNow.java
+++ DoNow011519/src/DoNow.java
@@ -1,4 +1,7 @@
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.Random;
+import java.util.Scanner;
 
 public class DoNow 
 {
@@ -11,42 +14,71 @@ public class DoNow
 
 		System.out.print(a);
 		System.out.print(b);
-		*/
+		 */
 
 		Account c = new Account("Jesse",10000000);
-		System.out.print(c.toString());
+		System.out.println(c);
+
+		Account d = new Account("Cindy", 0);
+		System.out.println(d);
+
+		c.takeOut(100);
+		/*
+		Scanner in = new Scanner(System.in);
+		System.out.println(in);
+
+		int[] array = new int[5];
+		System.out.println(array);
+
+		String[] str = new String[5];
+		System.out.println(str);
+
+		Random rand = new Random();
+		System.out.println(rand);
+		 */
+
 	}
 
 }
 
 class Account 
 {
-    private double value;
-    private String owner;
-    
-    public Account(String initOwner,double initValue)
-    {
-        value = initValue;
-        owner = initOwner;
-    }
+	private double value;
+	private String owner;
 
-    public void deposit(double in)
-    {
-        value += in;
-    }
+	public Account(String initOwner,double initValue)
+	{
+		value = initValue;
+		owner = initOwner;
+	}
 
-    public void takeOut(double out)
-    {
-        value -= out;
-    }
+	public void deposit(double in)
+	{
+		value += in;
+	}
 
-    public double check()
-    {
-        return value;
-    }
+	public void takeOut(double out)
+	{
+		value -= out;
+	}
 
-    public String getOwner()
-    {
-        return owner;
-    }
+	public double check()
+	{
+		return value;
+	}
+
+	public String getOwner()
+	{
+		return owner;
+	}
+
+
+	public String toString()
+	{
+		if (value > 10000)
+			return "Hello, " + owner + "! You now have $" + value + ", that's a lot of money :)";
+		else 
+			return "Hello, " + owner + "! You only have $" + value + ", work hard and earn some money! ";
+	}
+
 }
