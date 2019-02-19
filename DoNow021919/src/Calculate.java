d470d9fa97e193eff68792d2f8ba80e2f3bf0fa1
mgrddsj <Jesse_Xu@live.com>
2019-02-19 08:41:28 +0800

    Update Calculate.java

--- DoNow021919/src/Calculate.java
+++ DoNow021919/src/Calculate.java
@@ -1,9 +1,29 @@
 
-public class Calculate {
+public class Calculate
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		calculate(11111, 22222);
+	}
+	
+	public static int calculate(int n1, int n2)
+	{
+		return n1+n2;
 	}
 
+	public static double calculate(double n1, double n2)
+	{
+		return n1-n2;
+	}
+
+	public static double calculate(double n1, int n2)
+	{
+		return n1*n2;
+	}
+
+	public static String calculate(double n1, double n2, double n3)
+	{
+		return (n1/n2/n3) + "";
+	}
 }
