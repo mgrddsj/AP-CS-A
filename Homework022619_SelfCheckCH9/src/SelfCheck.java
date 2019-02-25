42ad01eb9bc6bb58a77a7f5236807c92d9d87507
mgrddsj <Jesse_Xu@live.com>
2019-02-26 07:30:06 +0800

    Homework

---
+++ Homework022619_SelfCheckCH9/src/SelfCheck.java
@@ -0,0 +1,51 @@
+
+public class SelfCheck 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+	public static void problem2()
+	{
+		/*
+		 * Overloading: Having two methods with the same name but different parameters in the same class. 
+		 * Overriding: Having a method that has the same name with the one in its super class. 
+		 */
+	}
+
+	public static void problem5()
+	{
+		//No. Because they are private. 
+		//Yes. Because they are public. 
+	}
+
+	public static void problem6()
+	{
+		/*
+		 * public UndergraduateStudent(String inputName)
+		 * {
+		 * 		super(inputName, 18);
+		 * 		year = 0;
+		 * }
+		 */
+	}
+
+	public static void problem7()
+	{
+		/*
+		 * public void setAge(int age)
+		 * {
+		 * 		super.age = age;
+		 * 		year++;
+		 * }
+		 */
+	}
+
+	public static void problem8()
+	{
+		
+	}
+
+}
