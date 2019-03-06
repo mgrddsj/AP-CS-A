d908463b3453e0ad2bf973f7b4939256cee93c87
mgrddsj <Jesse_Xu@live.com>
2019-03-06 08:51:26 +0800

    Today's code

---
+++ DoNow030619/src/TestFinal.java
@@ -0,0 +1,28 @@
+
+public class TestFinal 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+
+	final public static void testMethod()
+	{
+		System.out.println("Lalalalalalala");
+	}
+
+}
+
+final class TestFinal
+{
+	final public int a = 1;
+
+	final public void printSomething()
+	{
+		System.out.println(a);
+	}
+	
+}
+
