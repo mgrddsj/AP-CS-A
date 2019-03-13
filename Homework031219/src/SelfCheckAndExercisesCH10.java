62217f629c5b5930146630096be28d316bd4c326
mgrddsj <Jesse_Xu@live.com>
2019-03-13 08:52:00 +0800

    Update SelfCheckAndExercisesCH10.java

--- Homework031219/src/SelfCheckAndExercisesCH10.java
+++ Homework031219/src/SelfCheckAndExercisesCH10.java
@@ -124,4 +124,58 @@ public class SelfCheckAndExercisesCH10
 		//Change <int> to <Integet> will fix the problem. 
 	}
 
+	public static void q14()
+	{
+		//Wrapper classes are classes that provides a way to use primitive data types as objects.
+		//int is a primitive data type, Integer is the wrapper class of it. 
+		//int cannot be used as a data type for ArrayList, Integer can. 
+	}
+
+	public static void q15()
+	{
+		/*
+		 * a. [1, 2, 6, 8]
+		 * b. [10, 20, 30, 40, 50, 60]
+		 * c. [-4, 1, 4, 9, 16, 25, 36, 49, 64]
+		 */
+	}
+
+	public static void q16()
+	{
+		/*
+		 * a. [20, 10, 20, 30, 30, 20]
+		 * b. [8, 7, 8, 2, 9, 7, 4, 4, 8, 8]
+		 * c. [33, 28, 33, -1, 3, 28, 17, 9, 33, 17, -1, 33]
+		 */
+	}
+
+	public static void q17()
+	{
+		/*
+		 * a. [72, 20]
+		 * b. [1, 20, 18, 15, 11, 6]
+		 * c. [10, 90, 70, 40]
+		 */
+	}
+
+	public static void q18()
+	{
+		/*
+		 * a. [30, 20, 10]
+		 * b. [4, 7, 9, 2, 8]
+		 * c. [33, 9, 17, 28, 3, -1]
+		 */
+	}
+
+	public static ArrayList<String> swapPairs(ArrayList<String> strings)
+	{
+		for (int i=0; i<strings.size()/2; i++)
+		{
+			String temp = strings.get(i*2+1);
+			strings.set(i*2+1, strings.get(i*2));
+			strings.set(i*2, temp);
+		}
+		return strings;
+	}
+
 }
