0a52eb5896d45507c06e9ed7531023efe2db8a97
mgrddsj <Jesse_Xu@live.com>
2019-03-25 08:51:44 +0800

    Today's code

--- DoNow032219/src/BinarySearch.java
+++ DoNow032219/src/BinarySearch.java
@@ -11,55 +11,43 @@ public class BinarySearch
 		int[] array = new int[100];
 		for (int i=0; i<array.length; i++)
 		{
-			array[i] = rand.nextInt(1000) - 500;
+			array[i] = rand.nextInt(100);
 		}
 
 		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
 
-		System.out.println(binarySearch(array, 35));
+		System.out.println(binarySearch(array, 15));
 	}
 
 	public static int binarySearch(int[] array, int numToFind)
 	{
 		int index=-1;
-		boolean last = true;	//True: last is bigger, False: last is smaller. 
-		int loopCount = 0;
 		boolean found = false;
 		int i = array.length/2;
-		int count = 1;
+		int count = 2;
 
 		while (!found)
 		{
 			if (numToFind > array[i])
 			{
-				i += array.length/(Math.pow(array.length, count));
+				i += array.length/(Math.pow(2, count));
 				count++;
-				if (last == false)
-				{
-					loopCount++;
-				}
-				else 
-				{
-					loopCount = 0;
-				}
+			}
+			else if (numToFind < array[i])
+			{
+				i -= array.length/(Math.pow(2, count));
+				count++;
 			}
 			else 
 			{
-				i -= array.length/(Math.pow(array.length, count));
-				count++;
-				if (last == true)
-				{
-					loopCount++;
-				}
-				else 
-				{
-					loopCount = 0;
-				}
+				index = i;
+				found = true;
 			}
 
-			if (loopCount>5)
+			if (array.length/Math.pow(2, count) < 1)
 			{
-				break;
+				found = true;
 			}
 		}
 		return index;
