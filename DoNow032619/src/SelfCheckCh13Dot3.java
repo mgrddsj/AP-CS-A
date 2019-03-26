2be17ee2a09e8d50a4d9ba4a16ec1ec26869f53e
mgrddsj <Jesse_Xu@live.com>
2019-03-26 08:51:48 +0800

    Today's practices

---
+++ DoNow032619/src/SelfCheckCh13Dot3.java
@@ -0,0 +1,68 @@
+
+public class SelfCheckCh13Dot3 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+	public static void Q16()
+	{
+		//	O(N)
+		//	O(N)
+	}
+
+	public static void Q17()
+	{
+		//	Because binary search assumes that the array is sorted, and the middle index has the median value of the array. 
+	}
+
+	public static void Q18()
+	{
+		//	6
+	}
+
+	public static void Q19()
+	{
+		/*
+		 * 	a. 4; 4
+		 * 	b. 4, 6; 6
+		 * 	c. 4, 6, 7; 7
+		 * 	d. 4, 2, 0; 0
+		 */
+	}
+
+	public static void Q20()
+	{
+		//	4, 6, 5
+		//	Result: -1
+		//	the result does not make sense because the array is not sorted. 
+	}
+
+	public static void Q21()
+	{
+		/*
+		 * 	a. 7, 11, 9; 9
+		 * 	b. 7, 3, 5, 4; -1
+		 * 	c. 7, 11, 13, 14; 14
+		 * 	d. 7, 3, 5, 6; -1
+		 */
+	}
+
+	public static void Q22()
+	{
+		/* 
+		 * 	a. 7, 3, 1, 2; -1
+		 * 	b. 7, 3, 5, 6; 6
+		 * 	c. 7, 10, 8, 9; -1
+		 * 	d. 7, 3, 1, 0; -1
+		 */
+	}
+
+	public static void Q23()
+	{
+		
+	}
+
+}
